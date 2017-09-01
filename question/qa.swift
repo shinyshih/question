ee542c6a9f9392524fe6ce03c1f10495177accb6
“施馨檸” <shiny761118@gmail.com>
2017-09-01 15:07:21 +0800

    create a new qa game.

---
+++ question/qa.swift
@@ -0,0 +1,36 @@
+//
+//  qa.swift
+//  question
+//
+//  Created by 施馨檸 on 2017/9/1.
+//  Copyright © 2017年 施馨檸. All rights reserved.
+//
+
+import Foundation
+
+
+struct qna {
+    let question: String
+    let answer: String
+}
+
+let qnaArray = [qna.init(question: "地球目前總共有幾個自然衛星？", answer: "一個"),
+                qna.init(question: "日本著名的明治神宮位於？", answer: "東京"),
+                qna.init(question: "十三行文化遺址在台北縣哪裡發現？", answer: "八里"),
+                qna.init(question: "希臘神話中一年有幾季？", answer: "三季"),
+                qna.init(question: "行人過馬路的小綠人跑步號誌，是哪裡發明的？", answer: "台灣"),
+                qna.init(question: "藝人孫燕姿是哪一國人？", answer: "新加坡"),
+                qna.init(question: "Mickey Mouse的生日是幾號？", answer: "11月18日"),
+                qna.init(question: "地球與太陽之間的距離有多少公里？", answer: "一億五千萬"),
+                qna.init(question: "劉德華的英文名？", answer: "Andy"),
+                qna.init(question: "受驚嚇的長頸鹿，會有什麼動作？", answer: "把頭伸高"),
+                qna.init(question: "古時候說的地牛翻身，指的是哪一種天然災害？", answer: "地震"),
+                qna.init(question: "據說寫書法技巧好到能入入木三分的書法家是？", answer: "王羲之"),
+                qna.init(question: "月球的地心引力約為地球的多少？", answer: "六分之一"),
+                qna.init(question: "卡通櫻桃小丸子中的藤木同學被誰喜歡？", answer: "吉川綠"),
+                qna.init(question: "國王企鵝的孵蛋功作主要是由誰進行的？", answer: "爸爸"),
+                qna.init(question: "偶像團體5566共同參與哪一部戲劇的演出？", answer: "MVP情人"),
+                qna.init(question: "太陽早上是由哪邊升起？", answer: "東方"),
+                qna.init(question: "哪一項活動不需要肌肉、骨骼和關節一起作用？", answer: "發呆"),
+                qna.init(question: "貝多芬出生於？", answer: "法國"),
+                qna.init(question: "蘋果在哪一年推出了Apple I 個人電腦？", answer: "西元1976年")]
---
+++ questionTests/questionTests.swift
@@ -0,0 +1,36 @@
+//
+//  questionTests.swift
+//  questionTests
+//
+//  Created by 施馨檸 on 2017/9/1.
+//  Copyright © 2017年 施馨檸. All rights reserved.
+//
+
+import XCTest
+@testable import question
+
+class questionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
